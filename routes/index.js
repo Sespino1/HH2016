907bd90f5f14c8c910ef113a69dc75e3e9d1681d
Elizabeth <lizthompson14@gmail.com>
2016-10-29 15:52:03 -0400

    added routes for dashboard and merchant Select

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,16 @@ router.get('/login', function(req, res, next) {
   res.render('login', { title: 'Login', cards: [["1", "card1"], ["2", "card2"]] });
 });
 
+/* GET dashboard page. */
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'My Cards', cards: [["12345", "30.00"], ["67891", "25.00"]] });
+});
+
+/* GET merchantSelect page. */
+router.get('/merchantSelect', function(req, res, next) {
+  res.render('merchantSelect', { title: 'Merchant Selection', merchants:["Target", "Walmart"] });
+});
+
 /* GET redeemReward page. */
 router.get('/redeemReward', function(req, res, next) {
   res.render('redeemReward', { title: 'Redeem', redeemAmount: "25.00" });
