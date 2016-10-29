c9f9c1e5f7f9ad20497b101c11ef7a4751807d17
Elizabeth <lizthompson14@gmail.com>
2016-10-29 15:42:09 -0400

    added basic ejs files and routing

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Login', cards: [["1", "card1"], ["2", "card2"]] });
+});
+
+/* GET redeemReward page. */
+router.get('/redeemReward', function(req, res, next) {
+  res.render('redeemReward', { title: 'Redeem', redeemAmount: "25.00" });
+});
+
 module.exports = router;
